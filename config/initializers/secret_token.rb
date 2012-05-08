d207895d37418623e0fab23773d262f19a8cb617
Joan Lee <joan.eun.lee@gmail.com>
2012-05-07 21:34:42 -0400

    initial fear of commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '47d0f4ef31b6c3c449233c1a42fe9b6831febf5d499d06cbd83eb3fdd3688f42d4fbda4aa0fdd3d31b2af4970d077a1bd0082d1639e2a4e67722e697f619c89e'
